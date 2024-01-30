77b1f1ccefb74b51c37fc9a4f08d50569505471e
NjengaC <victorcyrus01@gmail.com>
2024-01-30 15:38:57 +0300

    post-order traversing

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,5 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 /*static size_t _height(const binary_tree_t *tree);*/
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
-
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 #endif /* _BINARY_TREES_H_ */
