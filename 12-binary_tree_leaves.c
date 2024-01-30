a0dc48f4a86051033472c00e2ad9b158b73d5d88
NjengaC <victorcyrus01@gmail.com>
2024-01-30 17:30:50 +0300

    leaves count

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: the tree
+ * Return: number of leaves
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (tree)
+	{
+		if (tree->left == NULL && tree->right == NULL)
+			count++;
+		else
+		{
+			count += binary_tree_leaves(tree->left);
+			count += binary_tree_leaves(tree->right);
+		}
+
+	}
+	return (count);
+}
