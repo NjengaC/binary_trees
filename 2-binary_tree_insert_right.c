54dc85e6de903aa08f5f0d20ba56c778e9baf1e8
NjengaC <victorcyrus01@gmail.com>
2024-01-30 13:41:43 +0300

    parent node

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new->n = value;
 	new->left = NULL;
 	new->right = NULL;
-	/*new->parent = parent;*/
+	new->parent = parent;
 
 	if (parent->right == NULL)
 		parent->right = new;
