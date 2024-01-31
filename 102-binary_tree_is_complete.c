2f0c9c4230977137f83bc2310a589df810c61bf4
NjengaC <victorcyrus01@gmail.com>
2024-01-31 19:50:26 +0300

    checkers

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,10 +1,22 @@
 #include "binary_trees.h"
-#include <stdio.h>
+size_t binary_tree_size(const binary_tree_t *tree);
+int is_complete_util(const binary_tree_t *tree, size_t index, size_t size);
+/**
+ * binary_tree_size - Measure the size of a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: Size of the tree, 0 if @tree is NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+}
 
 /**
- * binary_tree_is_complete - Check if a binary tree is complete
- * @tree: Pointer to the root node of the tree to check
- *
+ * binary_tree_is_complete - checks if the tree is complete
+ * @tree: root node of tree to check
  * Return: 1 if the tree is complete, 0 otherwise
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
@@ -12,36 +24,27 @@ int binary_tree_is_complete(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	int is_last_level = 0;
-	const binary_tree_t *current;
-	const binary_tree_t **queue = NULL;
-	int front = 0, rear = 0;
-	int size = 1024;
+	size_t size = binary_tree_size(tree);
 
-	queue = malloc(sizeof(binary_tree_t *) * size);
-	if (queue == NULL)
+	return (is_complete_util(tree, 0, size));
+}
+
+/**
+ * is_complete_util - Utility function to check if the tree is complete
+ * @tree: Pointer to the root node of the tree
+ * @index: Index of the current node
+ * @size: Total number of nodes in the tree
+ * Return: 1 if the tree is complete, 0 otherwise
+ */
+int is_complete_util(const binary_tree_t *tree, size_t index, size_t size)
+{
+	if (tree == NULL)
+		return (1);
+
+	if (index >= size)
 		return (0);
 
-	queue[rear++] = tree;
-
-	while (front < rear)
-	{
-		current = queue[front++];
-
-		if (current == NULL)
-		{
-			is_last_level = 1;
-			continue;
-		}
-
-		if (is_last_level)
-			return (0);
-
-		queue[rear++] = current->left;
-		queue[rear++] = current->right;
-	}
-
-	free(queue);
-
-	return (1);
+	return (is_complete_util(tree->left, 2 * index + 1, size) &&
+			is_complete_util(tree->right, 2 * index + 2, size));
 }
+
